d878c5260cc568de781f54084a3841c72337ec03
namazovanton <namazovanton@gmail.com>
2023-04-29 11:52:18 +0300

    Homework 31 upgraded

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -6,7 +6,7 @@ import pytest
 def access_token(client, django_user_model):
     # username = "Anton"
     # password = "LamborghiniHennessy30101998"
-    django_user_model.objects.create(username="Anton", password="LamborghiniHennessy30101998", role='admin')
+    django_user_model.objects.create_user(username="Anton", password="LamborghiniHennessy30101998", role='admin')
     response = client.post("/user/token/", {"username": "Anton",
                                             "password": "LamborghiniHennessy30101998"})
     print(response)
@@ -25,6 +25,6 @@ def access_token(client, django_user_model):
 def user_with_access_token(client, django_user_model):
     username = "Anton"
     password = "LamborghiniHennessy30101998"
-    test_user = django_user_model.objects.create(username=username, password=password, role='admin')
+    test_user = django_user_model.objects.create_user(username=username, password=password, role='admin')
     response = client.post("/user/token/", data={"username": username, "password": password})
     return test_user, response.data.get("access")
